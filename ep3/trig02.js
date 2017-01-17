588b2b119c5aed8529cbd4d2827dac14560e4e50
Ain <ain.salula@gmail.com>
2017-01-17 06:06:40 +0200

    a circle changing alpha (opacity)

--- ep3/trig02.js
+++ ep3/trig02.js
@@ -6,19 +6,20 @@ window.onload = function () {
     
     var centerY = height * .5,
         centerX = width * .5,
-        baseRadius = 100,
-        offset = 50,
+        baseAlpha = 0.5,
+        offset = 0.5,
         speed = .1,
         angle = 0;
     
     render();
 
     function render() {
-        var radius = baseRadius + Math.sin(angle) * offset;
+        var alpha = baseAlpha + Math.sin(angle) * offset;
 
+        context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
         context.clearRect(0, 0 , width, height);
         context.beginPath();
-        context.arc(centerX, centerY, radius, 0, Math.PI * 2, false);
+        context.arc(centerX, centerY, 100, 0, Math.PI * 2, false);
         context.fill();
 
         angle += speed;
