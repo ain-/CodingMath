c83e45a48012208cae2c9db650f4b3f1f9a82fcf
Ain <ain.salula@gmail.com>
2017-01-19 22:53:06 +0200

    spaceship with rotation and acceleration

--- ep10/main.js
+++ ep10/main.js
@@ -4,23 +4,26 @@ window.onload = function() {
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight,
         ship = particle.create(width / 2, height / 2, 0, 0),
-        thrust = vector.create(0, 0);
+        thrust = vector.create(0, 0),
+        angle = 0,
+        turningLeft = false,
+        turningRight = false,
+        thrusting = false;
 
     update();
 
     document.body.addEventListener("keydown", function(event) {
         switch(event.keyCode) {
             case 38: // up
-                thrust.setY(-0.1);
+                thrusting = true;
                 break;
             case 40: // down
-                thrust.setY(0.1);
                 break;
             case 37: // left
-                thrust.setX(-0.1);
+                turningLeft = true;
                 break;
             case 39: // right
-                thrust.setX(0.1);
+                turningRight = true;
                 break;
             default:
                 break;
@@ -30,16 +33,16 @@ window.onload = function() {
     document.body.addEventListener("keyup", function(event) {
         switch(event.keyCode) {
             case 38: // up
-                thrust.setY(0);
+                thrusting = false;
                 break;
             case 40: // down
                 thrust.setY(0);
                 break;
             case 37: // left
-                thrust.setX(0);
+                turningLeft = false;
                 break;
             case 39: // right
-                thrust.setX(0);
+                turningRight = false;
                 break;
             default:
                 break;
@@ -49,12 +52,41 @@ window.onload = function() {
     function update() {
         context.clearRect(0, 0, width, height);
 
+        if (turningLeft) {
+            angle -= 0.05;
+        }
+        if (turningRight) {
+            angle += 0.05;
+        }
+
+        thrust.setAngle(angle);
+
+        if (thrusting) {
+            thrust.setLength(0.1);
+        }
+        else {
+            thrust.setLength(0);
+        }
+
         ship.accelerate(thrust);
         ship.update();
 
+        context.save();
+        context.translate(ship.position.getX(), ship.position.getY());
+        context.rotate(angle);
+
         context.beginPath();
-        context.arc(ship.position.getX(), ship.position.getY(),  10, 0, Math.PI * 2, false);
-        context.fill();
+        context.moveTo(10, 0);
+        context.lineTo(-10, -7);
+        context.lineTo(-10, 7);
+        context.lineTo(10, 0);
+        if (thrusting) {
+            context.moveTo(-10, 0);
+            context.lineTo(-18, 0);
+        }
+        context.stroke();
+
+        context.restore();
 
         if (ship.position.getX() > width) {
             ship.position.setX(0);
