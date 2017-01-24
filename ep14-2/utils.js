aba5e0ab38d79cfebd5631683eefd4619972cc6d
Ain <ain.salula@gmail.com>
2017-01-24 22:50:17 +0200

    rectangle and point collision

---
+++ ep14-2/utils.js
@@ -0,0 +1,46 @@
+var utils = {
+    norm: function(value, min, max) {
+        return (value - min) / (max - min);
+    },
+
+    lerp: function(norm, min, max) {
+        return (max - min) * norm + min;
+    },
+
+    map: function(value, sourceMin, sourceMax, destMin, destMax) {
+        return lerp(norm(value, sourceMin, sourceMax), destMin, destMax);
+    },
+
+    clamp: function(value, min, max) {
+        return Math.min(Math.max(value, min), max);
+    },
+
+    distance: function(p0, p1) {
+        var dx = p1.x - p0.x,
+            dy = p1.y - p0.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    },
+
+     distanceXY: function(p0, p1) {
+        var dx = p1.x - p0.x,
+            dy = p1.y - p0.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    },
+
+    circleCollision: function(c0, c1) {
+        return utils.distance(c0, c1) <= c0.radius + c1.radius;
+    },
+
+    circlePointCollision: function(x, y, circle) {
+        return utils.distanceXY(x, y, circle.x, circle.y) < circle.radius;
+    },
+
+    pointInRect: function(x, y, rect) {
+        return utils.inRange(x, rect.x, rect.x + rect.width) &&
+               utils.inRange(y, rect.y, rect.y + rect.height);
+    },
+
+    inRange: function(value, min, max) {
+        return value >= min && value <= max;
+    }
+}
