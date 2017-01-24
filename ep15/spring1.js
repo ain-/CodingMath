7307d6428b50e7837046b947453e47c950ab9f80
Ain <ain.salula@gmail.com>
2017-01-25 00:58:18 +0200

    spring

---
+++ ep15/spring1.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight,
+        springPoint = vector.create(width / 2, height / 2),
+        weight = particle.create(Math.random() * width, Math.random() * height, 0, 0),
+        k = 0.1;
+
+    weight.radius = 20;
+
+    update();
+
+    function update() {
+        context.clearRect(0, 0, width, height);
+
+        var distance = springPoint.subtract(weight.position),
+            springForce = distance.multiply(k);
+        
+        weight.velocity.addTo(springForce);
+
+        weight.update();
+
+        context.beginPath();
+        context.arc(weight.position.getX(), weight.position.getY(), weight.radius,
+                0, Math.PI * 2, false);
+        context.fill();
+
+        context.beginPath();
+        context.arc(springPoint.getX(), springPoint.getY(), 4,
+                0, Math.PI * 2, false);
+        context.fill();
+
+        context.beginPath();
+        context.moveTo(weight.position.getX(), weight.position.getY());
+        context.lineTo(springPoint.getX(), springPoint.getY());
+        context.stroke();
+
+        requestAnimationFrame(update);
+    }
+
+};
+
+
