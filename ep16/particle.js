b577a84797a1a4dcc9695018eac735b71b2f9a53
Ain <ain.salula@gmail.com>
2017-01-25 02:35:42 +0200

    spring with fixed length

---
+++ ep16/particle.js
@@ -0,0 +1,29 @@
+var particle = {
+    position: null,
+    velocity: null,
+    mass: 1,
+    radius: 0,
+    bounce: -1,
+    friction: 1,
+
+    create: function(x, y, speed, direction, grav) {
+        var obj = Object.create(this);
+        obj.position = vector.create(x, y);
+        obj.velocity = vector.create(0, 0);
+        obj.velocity.setLength(speed);
+        obj.velocity.setLength(speed);
+        obj.velocity.setAngle(direction);
+        obj.gravity = vector.create(0, grav || 0);
+        return obj;
+    },
+
+    accelerate: function(accel) {
+        this.velocity.addTo(accel);
+    },
+
+    update: function() {
+        this.velocity.multiplyBy(this.friction);
+        this.velocity.addTo(this.gravity);
+        this.position.addTo(this.velocity);
+    }
+};
