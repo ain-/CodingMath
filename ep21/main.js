643a72cd29862e865d228b005352dfc1f07c7484
Ain <ain.salula@gmail.com>
2017-02-01 23:44:59 +0200

    drawing on targetContext (not working)

--- ep21/main.js
+++ ep21/main.js
@@ -1,8 +1,15 @@
 window.onload = function() {
     var canvas = document.getElementById("canvas"),
         context = canvas.getContext("2d"),
-        width = canvas.width = window.innerWidth,
-        height = canvas.height = window.innerHeight;
+        targetCanvas = document.getElementById("target"),
+        targetContext = targetCanvas.getContext("2d"),
+        width = canvas.width = targetCanvas.width = window.innerWidth,
+        height = canvas.height = targetCanvas.width = window.innerHeight,
+        p = particle.create(0, height / 2, 10, 0);
+
+    targetContext.beginPath();
+    targetContext.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false);
+    targetContext.fill();
 
 };
 
