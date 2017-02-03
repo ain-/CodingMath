4deee9e965e0c1b27f47ef67d13bef5fb205e125
Ain <ain.salula@gmail.com>
2017-02-03 20:27:20 +0200

    postcards in space

--- ep22/main.js
+++ ep22/main.js
@@ -5,18 +5,43 @@ window.onload = function() {
         height = canvas.height = window.innerHeight,
         p = particle.create(0, height / 2, 10, 0),
         fl = 300,
-        shapePos = {
-            x: 500,
-            y: 300,
-            z: 300
-        };
+        shapes = [],
+        numShapes = 100;
     
+    for (var i = 0; i < numShapes; i += 1) {
+        shapes[i] = {
+            x: utils.randomRange(-1000, 1000),
+            y: utils.randomRange(-1000, 1000),
+            z: utils.randomRange(0, 10000)
+        };
+    }
+
     context.translate(width / 2, height / 2);
 
-    var perspective = fl / (fl + shapePos.z);
-    context.translate(shapePos.x * perspective, shapePos.y * perspective);
-    context.scale(perspective, perspective);
-    context.fillRect(-100, -100, 200, 200);
+    update();
+
+    function update() {
+        context.clearRect(-width / 2, -height / 2, width, height);
+        for (var i = 0; i < numShapes; i += 1) {
+            var shape = shapes[i],
+                perspective = fl / (fl + shape.z);
+            
+            context.save();
+            context.translate(shape.x * perspective, shape.y * perspective);
+            context.scale(perspective, perspective);
+            context.fillRect(-100, -100, 200, 200);
+            context.restore();
+
+            shape.z += 5;
+            if (shape.z > 10000) {
+                shape.z = 0;
+            }
+        }
+        requestAnimationFrame(update);
+
+    }
+
+
    
 
 };
