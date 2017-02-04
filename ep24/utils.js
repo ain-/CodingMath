649e69929e0f68e07d4cd09c1d499128c8a766bb
Ain <ain.salula@gmail.com>
2017-02-04 13:18:28 +0200

    spiral

---
+++ ep24/utils.js
@@ -0,0 +1,102 @@
+var utils = {
+    norm: function(value, min, max) {
+        return (value - min) / (max - min);
+    },
+
+    lerp: function(norm, min, max) {
+        return (max - min) * norm + min;
+    },
+
+    map: function(value, sourceMin, sourceMax, destMin, destMax) {
+        return lerp(norm(value, sourceMin, sourceMax), destMin, destMax);
+    },
+
+    clamp: function(value, min, max) {
+        return Math.min(Math.max(value, Math.min(min, max)), Math.max(max, min));
+    },
+
+    distance: function(p0, p1) {
+        var dx = p1.x - p0.x,
+            dy = p1.y - p0.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    },
+
+     distanceXY: function(p0, p1) {
+        var dx = p1.x - p0.x,
+            dy = p1.y - p0.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    },
+
+    circleCollision: function(c0, c1) {
+        return utils.distance(c0, c1) <= c0.radius + c1.radius;
+    },
+
+    circlePointCollision: function(x, y, circle) {
+        return utils.distanceXY(x, y, circle.x, circle.y) < circle.radius;
+    },
+
+    pointInRect: function(x, y, rect) {
+        return utils.inRange(x, rect.x, rect.x + rect.width) &&
+               utils.inRange(y, rect.y, rect.y + rect.height);
+    },
+
+    inRange: function(value, min, max) {
+        return value >= Math.min(min, max) && value <= Math.max(min, max);
+    },
+
+    rangeIntersect: function(min0, max0, min1, max1) {
+        return Math.max(max0, min0) >= Math.min(min1, max1) && 
+               Math.min(min0, max0) <= Math.max(max1, min1);
+    },
+
+    rectIntersect: function(r0, r1) {
+        return utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
+               utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
+    },
+
+    randomRange: function(from, to) {
+        return Math.random() * (to - from) + from;
+    },
+
+    quadraticBezier: function(p0, p1, p2, t, pFinal) {
+        var pFinal = pFinal || {};
+        pFinal.x = Math.pow(1 - t, 2) * p0.x +
+                   (1 - t) * 2 * t * p1.x +
+                   t * t * p2.x;
+        pFinal.y = Math.pow(1 - t, 2) * p0.y + 
+                   (1 - t) * 2 * t * p1.y +
+                   t * t * p2.y;
+        return pFinal;
+    },
+
+    cubicBezier: function(p0, p1, p2, p3, t, pFinal) {
+        var pFinal = pFinal || {};
+        pFinal.x = Math.pow(1 - t, 3) * p0.x +
+                   Math.pow(1 - t, 2) * 3 * t * p1.x +        
+                   (1 - t) * 3 * t * t * p2.x +
+                   t * t * t * p3.x;
+        pFinal.y = Math.pow(1 - t, 3) * p0.y +
+                   Math.pow(1 - t, 2) * 3 * t * p1.y +        
+                   (1 - t) * 3 * t * t * p2.y +
+                   t * t * t * p3.y;
+        return pFinal;
+    },
+
+    multicurve: function(points, context) {
+        var p0, p1, midx, midy;
+
+        context.moveTo(points[0].x, points[0].y);
+
+        for(var i = 1; i < points.length - 2; i += 1) {
+            p0 = points[i];
+            p1 = points[i + 1];
+            midx = (p0.x + p1.x) / 2;
+            midy = (p0.y + p1.y) / 2;
+            context.quadraticCurveTo(p0.x, p0.y, midx, midy);
+        }
+
+        p0 = points[points.length - 2];
+        p1 = points[points.length - 1];
+        context.quadraticCurveTo(p0.x, p0.y, p1.x, p1.y);        
+    }
+}
