368472ad023ffe59afa2217ebfa6bb435536ab34
Ain <ain.salula@gmail.com>
2017-03-31 19:56:49 +0300

    27) easing simple

---
+++ ep27/main.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+  var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = canvas.width = window.innerWidth,
+    height = canvas.height = window.innerHeight,
+    target = {
+      x: width,
+      y: Math.random() * height
+    },
+    position = {
+      x: 0,
+      y: Math.random() * height
+    },
+    ease = 0.05;
+
+  update();
+
+  function update() {
+    context.clearRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(position.x, position.y, 10, 0, Math.PI * 2, false);
+    context.fill();
+
+    var dx = target.x - position.x,
+      dy = target.y - position.y,
+      vx = dx * ease,
+      vy = dy * ease;
+
+    position.x += vx;
+    position.y += vy;
+
+    requestAnimationFrame(update);
+  }
+
+};
