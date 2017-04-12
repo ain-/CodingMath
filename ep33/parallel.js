d5ae78a60ebc54ac0cf907e6bfeee4f689f837fb
Ain <ain.salula@gmail.com>
2017-04-12 03:59:24 +0300

    33) segment intersect

--- ep33/parallel.js
+++ ep33/parallel.js
@@ -10,15 +10,15 @@ window.onload = function() {
   },
   p1 = {
     x: 500,
-    y: 100
+    y: 500
   },
   p2 = {
-    x: 100,
-    y: 200
+    x: 300,
+    y: 100
   },
   p3 = {
-    x: 500,
-    y: 200
+    x: 100,
+    y: 600
   };
 
   context.beginPath();
@@ -28,7 +28,7 @@ window.onload = function() {
   context.lineTo(p3.x, p3.y);
   context.stroke();
 
-  var intersect = lineIntersect(p0, p1, p2, p3);
+  var intersect = segmentIntersect(p0, p1, p2, p3);
   if (intersect) {
     context.beginPath();
     context.arc(intersect.x, intersect.y, 20, 0, Math.PI * 2, false);
@@ -52,6 +52,35 @@ window.onload = function() {
       x: (B2*C1 - B1*C2) / denominator,
       y: (A1 * C2 - A2 * C1) / denominator
     };
+  }
+
+  function segmentIntersect(p0, p1, p2, p3) {
+    var A1 = p1.y - p0.y,
+      B1 = p0.x - p1.x,
+      C1 = A1 * p0.x + B1 * p0.y,
+      A2 = p3.y - p2.y,
+      B2 = p2.x - p3.x,
+      C2 = A2 * p2.x + B2 * p2.y,
+      denominator = A1 * B2 - A2 * B1;
+
+    if (denominator == 0) {
+      return null;
+    }
+
+    var intersectX = (B2*C1 - B1*C2) / denominator,
+      intersectY = (A1 * C2 - A2 * C1) / denominator,
+      rx0 = (intersectX - p0.x) / (p1.x - p0.x),
+      ry0 = (intersectY - p0.y) / (p1.y - p0.y);
+
+    if ((rx0 >= 0 && rx0 <= 1) || (ry0 >= 0 && ry0 <= 1)) {
+      return {
+        x: intersectX,
+        y: intersectY
+      };
+    }
+    else {
+      return null;
+    }
   }
 
 };
